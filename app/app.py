ddc7c536110ff4188ff4ba3de8018aa3725e64cd
mickey5m7 <52142064+mickey5m7@users.noreply.github.com>
2022-07-14 13:53:07 -0500

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#import plotly.express as px
+import streamlit as st
+#import os
+#from bokeh.plotting import figure
+#import altair as alt
+
+df = pd.read_csv('./penguins_size.csv')
+#st.write will help to write data in application and head() will show the head of data.
+st.write(df.head())
+
+st.title("Seaborn and Matplotlib Histograms")
+
+# Making Saeborn Chat
+st.subheader("Seaborn Chart")
+fig_sb, ax_sb = plt.subplots()
+ax_sb = sns.histplot(df['flipper_length_mm'])
+plt.xlabel('Flipper_length_mm')
+st.pyplot(fig_sb)
+
+# Making Matplotlib chart 
+st.subheader('Matploblib Chart')
+fig_mp, ax_mpl = plt.subplots()
+ax_mp = plt.hist(df['flipper_length_mm'])
+plt.xlabel('Flipper_length_mm')
+plt.ylabel('Count')
+st.pyplot(fig_mp)
